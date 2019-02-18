6570cc4d4c01473e98ea9f5075808382c7fe92d2
Matt Cool <matthewbcool@gmail.com>
2019-02-18 15:49:02 -0600

    nuked scratch setup and used cra boilerplate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react'
-import { Loop, Stage } from 'react-game-kit'
-import './App.css'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Loop>
-        <Stage>// Game specific components go here</Stage>
-      </Loop>
-    )
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
